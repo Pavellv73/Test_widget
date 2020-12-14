cebb118dd80da7e344a1672c19f06d80acc13e6b
Pavel Lysov <pasharus73@gmail.com>
2020-12-14 21:11:45 +0300

    [master] вывод сообщений

--- source/components/Widget/Widget.js
+++ source/components/Widget/Widget.js
@@ -1,26 +1,34 @@
 import './Widget.scss';
-import data from './data.json';
+import data from '../../../static/data.json';
 
 const widget = document.querySelector('.widget');
-const lenta = widget.querySelector('.widget__content');
+const content = widget.querySelector('.widget__content');
+const lenta = widget.querySelector('.widget__content-messageList');
 
 function getData() {
-  return Promise.all(data).then(response => response);
+  return data;
+}
+
+function generateItem(item) {
+  return `<div class="widget__content-message">
+        <span class="widget__content-message__title">${item.title}</span>
+        <p class="widget__content-message__text">${item.message}</p>
+        <p class="widget__content-message__author">${item.author}</p>
+        <p class="widget__content-message__date">${item.date}</p>
+        <a href="\`${item.link}\`" class="widget__content-message__link">Читать больше</a>
+      </div>`;
 }
 
 function initWidget() {
   const values = getData();
-  // const widget = document.querySelector('.widget');
-  // const title = widget.querySelector('.widget__message-title');
-  //
-  // values.map((item) => {
-  //   title.innerHTML = item.title;
-  // });
-  console.log(values, 'data');
+  values.data.forEach((item) => {
+    const msg = generateItem(item);
+    lenta.innerHTML += msg;
+  });
 }
 
 widget.addEventListener('click', () => {
-  lenta.classList.toggle('widget__content--visible');
+  content.classList.toggle('widget__content--visible');
 });
 
 document.addEventListener('DOMContentLoaded', initWidget());
